103ecffe45b7f89fdd53995e581167a81e86d722
wintercoder <winter_coder404@proton.me>
2025-12-02 23:12:23 -0500

    part one

---
+++ 2025/day_1/combination.py
@@ -0,0 +1,27 @@
+def main():
+    position = 50
+    zero_count = 0
+
+    with open("input.txt") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            direction = line[0]
+            distance= int(line[1:])
+            
+            if direction == 'L':
+                position -= distance
+            else:
+                position += distance
+
+            position %= 100
+            
+            if position == 0:
+                zero_count += 1
+
+    print(zero_count)
+
+if __name__ == "__main__":
+    main()
+    
